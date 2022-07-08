ea272752730a35a82f08074660a1decb84eb2c49
Arni Gunnarsson <awo066@rescomp1.hpc.in.bmrc.ox.ac.uk>
2022-07-08 14:46:00 +0100

    Traceback states and refactors

--- example.py
+++ example.py
@@ -5,17 +5,26 @@ bwt.insert([0, 1, 0, 0, 0, 1, 0, 0, 1])
 bwt.insert([0, 1, 1, 1, 0, 1, 1, 0, 1])
 bwt.insert([1, 1, 1, 0, 0, 1, 0, 1, 1])
 bwt.insert([1, 0, 0, 1, 0, 1, 1, 0, 1])
-# # print('nice')
-# bwt.print_sorting()
-# bwt.print_divergence()
 
 print(bwt.longest_prefix([0, 1, 1, 0, 0, 1, 0, 0, 1]))
 print(bwt.longest_prefix([1, 1, 1, 0, 0, 1, 0, 0, 1]))
-print(bwt.fastLS([1, 1, 1, 0, 0, 1, 0, 0, 1]))
-
+bwt.print_sorting()
+bw0 = bwt.fastLS([1, 1, 1, 0, 0, 1, 0, 0, 1])
+print(bw0)
+assert bw0 == [2] * 9
 
 bwt = DPBWT([1, 2, 3, 4, 5, 6, 7, 8], [1., 2., 3., 4., 5., 6., 7., 8.], 0.5)
 bwt.insert([0] * 8)
 bwt.insert([1] * 8)
-bwt.print_sorting()
-print(bwt.fastLS([1, 1, 1, 1, 0, 0, 0, 0]))
+# bwt.print_sorting()
+bw1 = bwt.fastLS([1, 1, 1, 1, 0, 0, 0, 0])
+print(bw1)
+assert bw1 == [1, 1, 1, 1, 0, 0, 0, 0]
+
+bw2 = bwt.fastLS([1, 1, 0, 0, 0, 0, 1, 1])
+print(bw2)
+assert bw2 == [1, 1, 0, 0, 0, 0, 1, 1]
+
+bw3 = bwt.fastLS([1, 0, 1, 0, 1, 0, 1, 1])
+print(bw3)
+assert bw3 == [1, 1, 1, 1, 1, 1, 1, 1]
