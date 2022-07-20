40c71995fb7478a7bc88aec0b35cf621478f2cc0
Arni Gunnarsson <awo066@compa044.hpc.in.bmrc.ox.ac.uk>
2022-07-20 17:53:58 +0100

    Add deletion

--- src/DPBWT.hpp
+++ src/DPBWT.hpp
@@ -2,7 +2,7 @@
 
 #include <memory>
 #include <vector>
-
+#include <unordered_map>
 
 
 class DPBWT {
@@ -11,6 +11,11 @@ private:
   // To access the linked lists in each column
   std::vector<std::unique_ptr<Node>> tops;
   std::vector<std::unique_ptr<Node>> bottoms;
+  std::unordered_map<int, int> ID_map;
+
+  Node* extend_node(Node* node, bool genotype, const int i);
+  bool extensible_by(State& s, const Node* t_next, const bool g, const int i);
+  bool genotype_interval_match(const int id1, const int id2, const int start, const int end);
 
 public:
   double ne;
@@ -28,20 +33,23 @@ public:
   DPBWT(std::vector<double> _physical_positions, std::vector<double> _genetic_positions, double _mutation_rate, double _ne);
   std::vector<double> site_sizes(std::vector<double> positions);
 
-  // Insertion and extention functions
+  // Insertion/deletion
   void insert(std::vector<bool> genotype);
-  Node* extend_node(Node* node, bool genotype, const int i);
-  std::tuple<bool, int> extensible_by(State& s, const Node* t_next, const bool g, const int i);
-  bool state_node_prefix_match(State state, int sample_ID);
-  bool genotype_interval_match(const int id1, const int id2, const int start, const int end);
+  void insert(std::vector<bool> genotype, int ID);
+  void delete_ID(int ID);
 
   // Algorithms
+  std::vector<std::tuple<int, int, double, double>> thread(std::vector<bool> genotype);
   std::vector<int> longest_prefix(std::vector<bool> genotype);
-  std::vector<int> fastLS(std::vector<bool> genotype);
+  std::vector<std::tuple<int, int>> fastLS(std::vector<bool> genotype);
   std::tuple<std::vector<double>, std::vector<double>> mutation_penalties();
   std::tuple<std::vector<double>, std::vector<double>> recombination_penalties();
 
+  // Aging
+  double age_segment_ML(const int id1, const int id2, const int start, const int end);
+  // (need to fix this)
+  double age_segment_bayes(const int id1, const int id2, const int start, const int end);
+
   // Debugging
   void print_sorting();
-  void print_divergence();
 };
