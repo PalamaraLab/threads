a6ccf651b6246976d767aef9a109bd4ce993cbf4
Arni Gunnarsson <awo066@rescomp1.hpc.in.bmrc.ox.ac.uk>
2023-01-19 15:57:22 +0000

    trim burn-in

--- src/Threads.hpp
+++ src/Threads.hpp
@@ -13,6 +13,12 @@ private:
   std::vector<std::unique_ptr<Node>> tops;
   std::vector<std::unique_ptr<Node>> bottoms;  // Ha, ha
 
+  // Burn-in quantities
+  int trim_pos_start;
+  int trim_pos_end;
+  int trim_pos_start_idx;
+  int trim_pos_end_idx;
+
   Node* extend_node(Node* node, bool genotype, const int i);
   bool extensible_by(State& s, const Node* t_next, const bool g, const int i);
   bool genotype_interval_match(const int id1, const int id2, const int start, const int end);
@@ -44,10 +50,19 @@ public:
   std::vector<std::vector<std::unique_ptr<Node>>> panel;
 
   // Constructors and utils
-  Threads(std::vector<double> _physical_positions, std::vector<double> _genetic_positions, double _mutation_rate, double ne, bool _sparse_sites, int _n_prune, bool _use_hmm) :
-    Threads(_physical_positions, _genetic_positions, _mutation_rate, std::vector<double>{ne}, std::vector<double>{0.0}, _sparse_sites, _n_prune, _use_hmm) {};
-  Threads(std::vector<double> _physical_positions, std::vector<double> _genetic_positions, double _mutation_rate, std::vector<double> ne, std::vector<double> ne_times, bool _sparse_sites, int _n_prune, bool _use_hmm);
+  Threads(std::vector<double> _physical_positions, std::vector<double> _genetic_positions,
+          double _mutation_rate, double ne, bool _sparse_sites, int _n_prune, bool _use_hmm,
+          int _burn_in_left, int _burn_in_right)
+      : Threads(_physical_positions, _genetic_positions, _mutation_rate, std::vector<double>{ne},
+                std::vector<double>{0.0}, _sparse_sites, _n_prune, _use_hmm, _burn_in_left,
+                _burn_in_right){};
+  Threads(std::vector<double> _physical_positions, std::vector<double> _genetic_positions,
+          double _mutation_rate, std::vector<double> ne, std::vector<double> ne_times,
+          bool _sparse_sites, int _n_prune, bool _use_hmm, int _burn_in_left, int _burn_in_right);
   std::tuple<std::vector<double>, std::vector<double>> site_sizes(std::vector<double> positions);
+
+  // More attributes
+  std::vector<double> trimmed_positions();
 
   // Insertion/deletion
   // Insert and assign generic ID
@@ -64,8 +79,8 @@ public:
   std::tuple<std::vector<int>, std::vector<int>, std::vector<double>, std::vector<int>> thread(const std::vector<bool>& genotype);
   std::tuple<std::vector<int>, std::vector<int>, std::vector<double>, std::vector<int>> thread(const int new_sample_ID, const std::vector<bool>& genotype);
   std::tuple<std::vector<int>, std::vector<int>, std::vector<double>, std::vector<int>>
-  Threads::remove_burn_in(std::vector<int> bp_starts, std::vector<int> target_IDs,
-                          std::vector<double> segment_ages, std::vector<int> het_sites);
+  remove_burn_in(std::vector<int> bp_starts, std::vector<int> target_IDs,
+                 std::vector<double> segment_ages, std::vector<int> het_sites);
   // std::tuple<std::vector<int>, std::vector<int>, std::vector<double>, std::vector<int>, std::vector<bool>> thread_with_mutations(const std::vector<bool>& genotype);
   // std::tuple<std::vector<int>, std::vector<int>, std::vector<double>, std::vector<int>, std::vector<bool>> thread_with_mutations(const int new_sample_ID, const std::vector<bool>& genotype);
   // std::vector<std::tuple<std::vector<int>, std::vector<int>, std::vector<double>>> thread_from_file(std::string file_path, int n_cycle);
