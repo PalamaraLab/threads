1510c06c7671d3aee00f6417421295b8e8746531
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2024-04-30 16:13:30 +0000

    test: regression test threads infer (#11)
    - Minimal rework of test added 1a28ca8 (branch wheels_etc where issue discovered) including reference data
    - Also includes devcontainer changes to debug/test in pylance

--- test/test_regression.py
+++ test/test_regression.py
@@ -1,66 +1,60 @@
-import subprocess
-import h5py
 import numpy as np
+import h5py
+import subprocess
+import tempfile
 
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
 
-
-def run_threads_infer():
+def _run_threads_infer(outfile: Path):
+    """
+    Generate .threads file from example data
+    """
+    dir = BASE_DIR / 'example'
     command = [
         "threads", "infer",
-        "--pgen", str(BASE_DIR / 'example' / 'example_data.pgen'),
-        "--map_gz", str(BASE_DIR / 'example' / 'example_data.map'),
-        "--demography", str(BASE_DIR / 'example' / 'Ne10000.demo'),
-        "--out", str(BASE_DIR / 'example' / 'example_data.threads'),
+        "--pgen", str(dir / 'example_data.pgen'),
+        "--map_gz", str(dir / 'example_data.map'),
+        "--demography", str(dir / 'Ne10000.demo'),
+        "--out", str(outfile),
     ]
 
-    return subprocess.run(command, cwd=BASE_DIR, capture_output=True, text=True)
+    return subprocess.run(
+        command,
+        cwd=BASE_DIR,
+        capture_output=True,
+        text=True
+    )
 
 
-def run_threads_convert():
-    command = [
-        "threads", "convert",
-        "--threads", str(BASE_DIR / 'example' / 'example_data.threads'),
-        "--argn", str(BASE_DIR / 'example' / 'example_data.argn'),
-    ]
+def test_infer_consistency():
+    """
+    Regression check for difference in output from threads infer
 
-    return subprocess.run(command, cwd=BASE_DIR, capture_output=True, text=True)
+    Reference data created April 19 2024 after apparently innocuous C++ changed output.
+    """
+    with tempfile.TemporaryDirectory() as tmpdir:
+        # Regenerate threads infer output
+        generated_threads = Path(tmpdir) / "test_infer_consistency.threads"
+        result_infer = _run_threads_infer(generated_threads)
+        assert result_infer.returncode == 0, f"Script did not run successfully: {result_infer.stderr}"
+        assert generated_threads.is_file()
 
+        # Compare against reference.threads data generated from examples dir
+        reference_threads = BASE_DIR / 'test' / 'data' / 'reference.threads'
+        assert reference_threads.is_file()
+        with h5py.File(generated_threads, 'r') as gen_file, h5py.File(reference_threads, 'r') as ref_file:
 
-def test_threads_on_example_data():
-    result_infer = run_threads_infer()
-    assert result_infer.returncode == 0, f"Script did not run successfully: {result_infer.stderr}"
-    generated_threads = BASE_DIR / 'example' / 'example_data.threads'
-    assert generated_threads.is_file()
+            # Compare attributes in the root of the files - the only attribute in this file should be datetime_created
+            assert gen_file.attrs.keys() == ref_file.attrs.keys()
 
-    result_convert = run_threads_convert()
-    assert result_convert.returncode == 0, f"Script did not run successfully: {result_convert.stderr}"
-    generated_argn = BASE_DIR / 'example' / 'example_data.argn'
-    assert generated_argn.is_file()
+            # Compare the names of datasets
+            assert set(gen_file.keys()) == set(ref_file.keys())
 
-    reference_threads = BASE_DIR / 'test' / 'data' / 'reference.threads'
-    assert reference_threads.is_file()
+            for dataset_name in gen_file.keys():
+                dset_gen = gen_file[dataset_name]
+                dset_ref = ref_file[dataset_name]
 
-    reference_argn = BASE_DIR / 'test' / 'data' / 'reference.argn'
-    assert reference_argn.is_file()
-
-    with h5py.File(generated_threads, 'r') as gen_file, h5py.File(reference_threads, 'r') as ref_file:
-
-        # Compare attributes in the root of the files - the only attribute in this file should be datetime_created
-        assert gen_file.attrs.keys() == ref_file.attrs.keys()
-
-        # Compare the names of datasets
-        assert set(gen_file.keys()) == set(ref_file.keys())
-
-        for dataset_name in gen_file.keys():
-            dset_gen = gen_file[dataset_name]
-            dset_ref = ref_file[dataset_name]
-
-            print('\n\n###')
-            print(dataset_name)
-            print(dset_gen.shape)
-
-            # assert dset_gen.shape == dset_ref.shape
-            # assert np.allclose(dset_gen, dset_ref)
+                assert dset_gen.shape == dset_ref.shape
+                assert np.allclose(dset_gen, dset_ref)
