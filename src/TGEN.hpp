71e184328b8df79e3d06f6d284e9b2db74c41fc7
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2024-05-30 13:09:10 +0000

    refactor: resolve TGEN hpp/cpp warnings (#22)
    * Reorder class to methods-first
    * Resolve size_t/int with easiest fix where possible
    * Remove commented-out Eigen-based code; may revisit later
    * Doxygen-friendly header comments
    * Clang format

--- src/TGEN.hpp
+++ src/TGEN.hpp
@@ -6,27 +6,22 @@
 #include <memory>
 #include <vector>
 
-
-class TGENImpl; // Forward declaration of the implementation class
+// Forward declaration of the implementation class
 // Using PIMPL idiom to prevent libraries that #include "TGEN.hpp" needing to see boost/icl headers
+class TGENImpl;
 
 class TGEN {
-
-private:
-  std::unique_ptr<TGENImpl> pimpl; // Pointer to implementation
-
 public:
-  // Constructors
   TGEN(std::vector<int> _positions, std::vector<std::vector<int>> _bp_starts,
        std::vector<std::vector<int>> _target_IDs, std::vector<std::vector<int>> _het_sites);
+  ~TGEN();
 
-  ~TGEN(); // Define the destructor for proper deletion of pimpl
-
-  std::vector<std::vector<bool>>& query(int start_pos, int end_pos, const std::vector<int>& samples);
-
-  // Deprecated Eigen version:
-  // Eigen::MatrixXi& query(const int start_pos, const int end_pos, const std::vector<int>& samples);
+  std::vector<std::vector<bool>>& query(int start_pos, int end_pos,
+                                        const std::vector<int>& samples);
   void clear_cache();
+
+private:
+  std::unique_ptr<TGENImpl> pimpl; ///< Pointer to implementation
 };
 
 #endif // THREADS_ARG_TGEN_HPP
