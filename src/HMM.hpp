b2cfef774ae365ef9a0ce086a8b18865820bd4fb
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2024-05-30 12:56:11 +0000

    refactor: fix HMM.hpp/cpp warnings (#22)
    * Reorder class to methods-first.
    * Doxygen-friendly header comments.
    * Clang format
    * Warnings picked up shadowed variable in compute_expected_times
    * Resolve size_t/int with easiest fix where possible
    * Warnings picked up unused variables (e.g. `trans` line 61)
    * Added exception for possible data conversion loss in `breakpoints`

--- src/HMM.hpp
+++ src/HMM.hpp
@@ -1,32 +1,31 @@
-#ifndef DEMOGRAPHY
-#define DEMOGRAPHY
 #include "Demography.hpp"
-#endif // DEMOGRAPHY
 
-#include <vector>
 #include <iostream>
+#include <vector>
 
-
-// This class contains the PSMC-like algorithm used to break up segments for small-N inference
+/// This class contains the PSMC-like algorithm used to break up segments for small-N inference
 class HMM {
+public:
+  HMM(Demography demography, std::vector<double> bp_sizes, std::vector<double> cm_sizes,
+      double mutation_rate, int K);
+  HMM() = default;
+
+  void compute_recombination_scores(std::vector<double> cm_sizes);
+  void compute_mutation_scores(std::vector<double> bp_sizes, double mutation_rate);
+  std::vector<double> compute_expected_times(Demography demography, int K);
+  std::vector<int> breakpoints(std::vector<bool> observations, int start);
+
 public:
   // HMM data
   int num_states = 0;
   std::vector<double> expected_times;
 
-  std::vector<std::vector<double>> non_transition_score;
-  std::vector<std::vector<double>> transition_score;
-  std::vector<std::vector<double>> hom_score;
-  std::vector<std::vector<double>> het_score;
-  void compute_recombination_scores(std::vector<double> cm_sizes);
-  void compute_mutation_scores(std::vector<double> bp_sizes, double mutation_rate);
-  std::vector<double> compute_expected_times(Demography demography, int K);
-
   // HMM working quantities
   std::vector<std::vector<double>> trellis;
   std::vector<std::vector<unsigned short>> pointers;
 
-  HMM(Demography demography, std::vector<double> bp_sizes, std::vector<double> cm_sizes, double mutation_rate, int K);
-  HMM() {};
-  std::vector<int> breakpoints(std::vector<bool> observations, int start);
+  std::vector<std::vector<double>> non_transition_score;
+  std::vector<std::vector<double>> transition_score;
+  std::vector<std::vector<double>> hom_score;
+  std::vector<std::vector<double>> het_score;
 };
