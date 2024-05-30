ae19ee36ac2e32b612982648c2aae3d064e5dc31
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2024-05-30 12:56:11 +0000

    refactor: fix HMM.hpp/cpp warnings (#22)
    * Reorder class to methods-first.
    * Doxygen-friendly header comments.
    * Clang format
    * Warnings picked up shadowed variable in compute_expected_times
    * Resolve size_t/int with easiest fix where possible
    * Warnings picked up unused variables (e.g. `trans` line 61)
    * Added exception for possible data conversion loss in `breakpoints`

--- src/HMM.cpp
+++ src/HMM.cpp
@@ -1,8 +1,8 @@
 #include "HMM.hpp"
+
 #include <boost/math/distributions/exponential.hpp>
 #include <iostream>
 #include <vector>
-
 
 HMM::HMM(Demography demography, std::vector<double> bp_sizes, std::vector<double> cm_sizes,
          double mutation_rate, int K)
@@ -12,7 +12,8 @@ HMM::HMM(Demography demography, std::vector<double> bp_sizes, std::vector<double
   compute_recombination_scores(cm_sizes);
   compute_mutation_scores(bp_sizes, mutation_rate);
 
-  for (int i = 0; i < bp_sizes.size(); i++) {
+  // FIXME Alex review with Arni, worth reserving up front or using emplace_back?
+  for (std::size_t i = 0; i < bp_sizes.size(); i++) {
     std::vector<double> trellis_row(num_states, 0.0);
     std::vector<unsigned short> pointer_row(num_states, 0);
     trellis.push_back(trellis_row);
@@ -21,19 +22,19 @@ HMM::HMM(Demography demography, std::vector<double> bp_sizes, std::vector<double
 }
 
 std::vector<double> HMM::compute_expected_times(Demography demography, const int K) {
-  std::vector<double> expected_times;
+  std::vector<double> result;
   double k = static_cast<double>(num_states);
   boost::math::exponential e;
 
   for (int i = 1; i <= K; i++) {
     double t = demography.std_to_gen(quantile(e, (i - 0.5) / k));
-    expected_times.push_back(t);
+    result.push_back(t);
   }
-  return expected_times;
+  return result;
 }
 
 void HMM::compute_recombination_scores(std::vector<double> cm_sizes) {
-  for (int i = 0; i < cm_sizes.size(); i++) {
+  for (std::size_t i = 0; i < cm_sizes.size(); i++) {
     non_transition_score.push_back(std::vector<double>());
     transition_score.push_back(std::vector<double>());
     for (int k = 0; k < num_states; k++) {
@@ -51,14 +52,14 @@ void HMM::compute_recombination_scores(std::vector<double> cm_sizes) {
 }
 
 void HMM::compute_mutation_scores(std::vector<double> bp_sizes, double mutation_rate) {
-  for (int i = 0; i < bp_sizes.size(); i++) {
+  for (std::size_t i = 0; i < bp_sizes.size(); i++) {
     hom_score.push_back(std::vector<double>());
     het_score.push_back(std::vector<double>());
     for (int k = 0; k < num_states; k++) {
       double t = expected_times[k];
+
       // TODO: use mean-bp sizes here as in the main algorithm
       const double l = 2. * mutation_rate * bp_sizes[i] * t;
-      const double trans = std::log1p(-std::exp(-l));
 
       // log-prob of mutating
       het_score[i].push_back(std::log1p(-std::exp(-l)));
@@ -75,17 +76,21 @@ void HMM::compute_mutation_scores(std::vector<double> bp_sizes, double mutation_
 std::vector<int> HMM::breakpoints(std::vector<bool> observations, int start) {
   // Viterbi
   // Initialize
-  int neighborhood_size = observations.size();
+  int neighborhood_size = static_cast<int>(observations.size());
   std::vector<unsigned short> z(neighborhood_size);
-  int end = start + neighborhood_size;
   for (int i = 0; i < num_states; i++) {
     double score = observations[0] ? het_score[start][i] : hom_score[start][i];
     trellis[start][i] = score;
   }
 
+  // FIXME review with Arni, defensive check for assignment to 'k' below
+  if (num_states > std::numeric_limits<unsigned short>::max()) {
+    throw std::runtime_error("Unable to store breakpoints for more than 2^16 states");
+  }
+
   // Main routine
-  double score;
-  unsigned short running_argmax;
+  double score = 0.0;
+  unsigned short running_argmax = 0;
   for (int j = 1; j < neighborhood_size; j++) {
     for (int i = 0; i < num_states; i++) {
       double running_max = 0;
@@ -98,7 +103,7 @@ std::vector<int> HMM::breakpoints(std::vector<bool> observations, int start) {
 
         if (score > running_max || k == 0) {
           running_max = score;
-          running_argmax = k;
+          running_argmax = static_cast<unsigned short>(k);
         }
       }
       trellis[j + start][i] = running_max;
@@ -113,7 +118,7 @@ std::vector<int> HMM::breakpoints(std::vector<bool> observations, int start) {
     double s = trellis[start + neighborhood_size - 1][k];
     if (s > running_max) {
       running_max = s;
-      argmax = k;
+      argmax = static_cast<unsigned short>(k);
     }
   }
 
