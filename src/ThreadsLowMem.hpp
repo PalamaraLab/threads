9d950470407f41dae3d1fa11022f51febaf7fd4c
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2024-05-30 13:19:57 +0000

    refactor: resolve ThreadsLowMem hpp/cpp warnings (#22)
    * Reorder class to methods-first and separate variables from methods
    * Ensure all member variables initialised
    * Remove commented out member variables
    * Ensure member variables initialised in correct order
    * Resolve size_t/int warnings
    * Replace C-ctyle casts
    * Parenthesis to confirm operator precedence
    * Clang format
    * Some review comments to pick up with regard to parenthesis and commented out code

--- src/ThreadsLowMem.hpp
+++ src/ThreadsLowMem.hpp
@@ -1,52 +1,17 @@
 #ifndef THREADS_ARG_THREADS_LOW_MEM_HPP
 #define THREADS_ARG_THREADS_LOW_MEM_HPP
 
+#include "Demography.hpp"
 #include "Matcher.hpp"
 #include "ThreadsFastLS.hpp"
 #include "ViterbiLowMem.hpp"
-#include "Demography.hpp"
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
 class ThreadsLowMem {
-
-private:
-  Demography demography;
-
-  // 2. HMM quantites
-  int hmm_sites_processed;
-  std::unordered_map<int, ViterbiState> hmms;
-  int match_group_idx;
-  std::vector<MatchGroup> match_groups;
-
-  // 3. Path segment and path dating quantites
-  HMM psmc;
-  // std::vector<double> cm_sizes;
-  // std::vector<double> bp_sizes;
-  int het_sites_processed;
-  int n_hmm_samples = 100;
-  int hmm_min_sites = 10;
-
 public:
-  // This object will only run the HMM for these ids
-  std::vector<int> target_ids;
-  std::unordered_map<int, double> expected_branch_lengths;
-  double mean_bp_size;
-  std::unordered_map<int, int> segment_indices;
-  std::unordered_map<int, ViterbiPath> paths;
-  int num_samples;
-  int num_sites;
-  double mutation_rate;
-  std::vector<double> physical_positions;
-  std::vector<double> genetic_positions;
-  std::vector<double> bp_sizes;
-  std::vector<double> cm_sizes;
-  std::vector<double> bp_boundaries;
-  std::vector<double> cm_boundaries;
-  bool sparse;
-
   ThreadsLowMem(const std::vector<int> _target_ids, const std::vector<double>& _physical_positions,
                 const std::vector<double>& _genetic_positions, std::vector<double> ne,
                 std::vector<double> ne_times, double _mutation_rate, bool _sparse);
@@ -78,6 +43,39 @@ public:
   std::tuple<std::vector<std::vector<int>>, std::vector<std::vector<int>>,
              std::vector<std::vector<double>>, std::vector<std::vector<int>>>
   serialize_paths();
+
+public:
+  // This object will only run the HMM for these ids
+  std::vector<int> target_ids;
+  std::unordered_map<int, double> expected_branch_lengths;
+  double mean_bp_size = 0.0;
+  std::unordered_map<int, int> segment_indices;
+  std::unordered_map<int, ViterbiPath> paths;
+  int num_samples = 0;
+  int num_sites = 0;
+  double mutation_rate = 0.0;
+  std::vector<double> physical_positions;
+  std::vector<double> genetic_positions;
+  std::vector<double> bp_sizes;
+  std::vector<double> cm_sizes;
+  std::vector<double> bp_boundaries;
+  std::vector<double> cm_boundaries;
+  bool sparse = false;
+
+private:
+  Demography demography;
+
+  // 2. HMM quantites
+  int hmm_sites_processed = 0;
+  std::unordered_map<int, ViterbiState> hmms;
+  int match_group_idx = 0;
+  std::vector<MatchGroup> match_groups;
+
+  // 3. Path segment and path dating quantites
+  HMM psmc;
+  int het_sites_processed = 0;
+  int n_hmm_samples = 100;
+  int hmm_min_sites = 10;
 };
 
 #endif // THREADS_ARG_THREADS_LOW_MEM_HPP
