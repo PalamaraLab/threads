661871047cb3ff46e197284d8525085fa571e91f
Arni Gunnarsson <awo066@rescomp2.hpc.in.bmrc.ox.ac.uk>
2024-08-17 17:40:47 +0100

    Add function to slice threading instructions + test

--- src/threads_arg/utils.py
+++ src/threads_arg/utils.py
@@ -21,34 +21,92 @@ import pandas as pd
 import warnings
 
 def decompress_threads(threads):
+    """
+    Read in threading instructions for the purpose of converting to tskit/arg-needle format.
+    """
     f = h5py.File(threads, "r")
 
-    samples, thread_starts = f["samples"][:, 0], f["samples"][:, 1]
+    samples, thread_starts, mut_starts = f["samples"][:, 0], f["samples"][:, 1], f["samples"][:, 2]
     positions = f['positions'][...]
     flat_ids, flat_bps = f['thread_targets'][:, :-1], f['thread_targets'][:, -1]
     flat_ages = f['thread_ages'][...]
-    try:
-        arg_range = f['arg_range'][...]
-    except KeyError:
-        arg_range = [np.nan, np.nan]
+    flat_hets = f['het_sites'][...]
+    arg_range = f['arg_range'][...]
 
     threading_instructions = []
-    for i, start in enumerate(thread_starts):
+    mutations = []
+    for i, (start, start_mut) in enumerate(zip(thread_starts, mut_starts)):
         if i == len(thread_starts) - 1:
             ids = flat_ids[start:]
             bps = flat_bps[start:]
             ages = flat_ages[start:]
+            muts = flat_hets[start_mut:]
         else:
             ids = flat_ids[start:thread_starts[i + 1]]
             bps = flat_bps[start:thread_starts[i + 1]]
             ages = flat_ages[start:thread_starts[i + 1]]
+            muts = flat_hets[start_mut:mut_starts[i + 1]]
         threading_instructions.append((bps, ids, ages))
+        mutations.append(muts)
+    
     return {
         "threads": threading_instructions,
         "samples": samples,
         "positions": positions,
-        "arg_range": arg_range
+        "arg_range": arg_range,
+        "mutations": mutations
     }
+
+
+def slice_threading_instructions(threading_data, start_pos, end_pos):
+    """
+    Slice threading instructions to only include data within the region
+    start_pos-end_pos (both inclusive)
+    TODO: make threading instructions be a (ideally c++-side) class object and not a random dict.
+    """
+    if start_pos >= end_pos:
+        raise ValueError(f"Need start_pos < end_pos, found start_pos={start_pos} and end_pos={end_pos}")
+
+
+    old_range = threading_data["arg_range"]
+    if start_pos < old_range[0] or start_pos >= old_range[1]:
+        raise ValueError(f"Need start_pos within arg range, found start_pos={start_pos} and range {old_range[0]}-{old_range[1]}")
+    if end_pos <= old_range[0] or end_pos > old_range[1]:
+        raise ValueError(f"Need end_pos within arg range, found end_pos={end_pos} and range {old_range[0]}-{old_range[1]}")
+
+    positions = threading_data["positions"]
+
+    new_threading_instructions = []
+    new_mutations = []
+    new_arg_range = [start_pos, end_pos]
+    new_samples = threading_data["samples"]
+    new_positions = positions[(start_pos <= positions) & (positions <= end_pos)]
+
+    for muts, (bps, ids, ages) in zip(threading_data["mutations"], threading_data["threads"]):
+        new_mutations.append(muts[(start_pos <= muts) & (muts <= end_pos)])
+
+        if len(bps) == 0:
+            new_threading_instructions.append((np.array([]), np.array([]), np.array([])))
+            continue
+
+        first_idx = np.nonzero(bps <= start_pos)[0][-1]
+        if bps[-1] <= end_pos:
+            last_idx = len(bps)
+        else:
+            last_idx = np.nonzero(end_pos <= bps)[0][0]
+        new_bps = np.concatenate([np.array([start_pos]), bps[(first_idx + 1):last_idx]])
+        new_ids = ids[first_idx:last_idx]
+        new_ages = ages[first_idx:last_idx]
+        new_threading_instructions.append((new_bps, new_ids, new_ages))
+
+    return {
+        "threads": new_threading_instructions,
+        "samples": new_samples,
+        "positions": new_positions,
+        "arg_range": new_arg_range,
+        "mutations": new_mutations
+    }
+
 
 def read_map_gz(map_gz):
     """
---
+++ test/test_threading_instructions.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pytest
+
+import threads_arg
+
+def dummy_threading_instructions():
+    return {
+        # the "threads" are a list of 3-tuples lists of segment starts, threading targets and segment ages
+        "threads": [(np.array([]), np.array([]), np.array([])), 
+                    (np.array([4, 8]), np.array([0, 0]), np.array([2.5, 3.0])),
+                    (np.array([4, 10, 15]), np.array([0, 1, 0]), np.array([1.0, 3.0, 6.0]))],
+        "samples": np.array([0, 1, 2]),
+        "positions": np.array([4, 6, 8, 10, 15]),
+        "arg_range": np.array([4, 20]),
+        "mutations": [np.array([4, 6, 10]),
+                      np.array([4, 8, 15]),
+                      np.array([6, 10])]
+    }
+
+def threading_instructions_are_equal(t1, t2):
+    try:
+        assert len(t1["threads"]) == len(t2["threads"])
+
+        for (bp1, id1, age1), (bp2, id2, age2) in zip(t1["threads"], t2["threads"]):
+            assert (bp1 == bp2).all()
+            assert (id1 == id2).all()
+            assert (age1 == age2).all()
+
+        for mut1, mut2 in zip(t1["mutations"], t2["mutations"]):
+            assert (mut1 == mut2).all()
+
+        assert (t1["samples"] == t2["samples"]).all()
+        assert (t1["arg_range"] == t2["arg_range"]).all()
+        assert (t1["positions"] == t2["positions"]).all()
+        return True
+    except AssertionError:
+        return False
+
+def test_slice():
+    """
+    Test the threading instructions slice function
+    """
+    tt = dummy_threading_instructions()
+
+    # Test illegal intervals
+    with pytest.raises(ValueError):
+        sliced = threads_arg.utils.slice_threading_instructions(tt, 3, 4)
+    with pytest.raises(ValueError):
+        sliced = threads_arg.utils.slice_threading_instructions(tt, 4, 4)
+    with pytest.raises(ValueError):
+        sliced = threads_arg.utils.slice_threading_instructions(tt, 4, 21)
+    
+    # Test identity on full interval
+    # import pdb
+    # pdb.set_trace()
+    assert threading_instructions_are_equal(threads_arg.utils.slice_threading_instructions(tt, 4, 20), tt)
+
+    # Test interval starting at arg range start and ending between sites
+    expected = {
+        "threads": [(np.array([]), np.array([]), np.array([])), 
+                    (np.array([4]), np.array([0]), np.array([2.5])),
+                    (np.array([4]), np.array([0]), np.array([1.0]))],
+        "samples": np.array([0, 1, 2]),
+        "positions": np.array([4, 6]),
+        "arg_range": np.array([4, 7]),
+        "mutations": [np.array([4, 6]),
+                      np.array([4]),
+                      np.array([6])]
+    }
+    assert threading_instructions_are_equal(threads_arg.utils.slice_threading_instructions(tt, 4, 7), expected)
+   
+    # Test interval starting at arg range start and ending on a site 
+    expected = {
+        "threads": [(np.array([]), np.array([]), np.array([])), 
+                    (np.array([4, 8]), np.array([0, 0]), np.array([2.5, 3.0])),
+                    (np.array([4]), np.array([0]), np.array([1.0]))],
+        "samples": np.array([0, 1, 2]),
+        "positions": np.array([4, 6, 8]),
+        "arg_range": np.array([4, 8]),
+        "mutations": [np.array([4, 6]),
+                    np.array([4, 8]),
+                    np.array([6])]
+    }
+    assert threading_instructions_are_equal(threads_arg.utils.slice_threading_instructions(tt, 4, 8), expected)
+
+    # Test interval starting at a site and ending at the arg range end
+    expected = {
+        "threads": [(np.array([]), np.array([]), np.array([])), 
+                    (np.array([6, 8]), np.array([0, 0]), np.array([2.5, 3.0])),
+                    (np.array([6, 10, 15]), np.array([0, 1, 0]), np.array([1.0, 3.0, 6.0]))],
+        "samples": np.array([0, 1, 2]),
+        "positions": np.array([6, 8, 10, 15]),
+        "arg_range": np.array([6, 20]),
+        "mutations": [np.array([6, 10]),
+                    np.array([8, 15]),
+                    np.array([6, 10])]
+    }
+    assert threading_instructions_are_equal(threads_arg.utils.slice_threading_instructions(tt, 6, 20), expected)
+
+    # Test interval starting and ending between sites
+    expected = {
+        "threads": [(np.array([]), np.array([]), np.array([])), 
+                    (np.array([7, 8]), np.array([0, 0]), np.array([2.5, 3.0])),
+                    (np.array([7, 10, 15]), np.array([0, 1, 0]), np.array([1.0, 3.0, 6.0]))],
+        "samples": np.array([0, 1, 2]),
+        "positions": np.array([8, 10, 15]),
+        "arg_range": np.array([7, 19]),
+        "mutations": [np.array([10]),
+                    np.array([8, 15]),
+                    np.array([10])]
+    }
+    assert threading_instructions_are_equal(threads_arg.utils.slice_threading_instructions(tt, 7, 19), expected)
