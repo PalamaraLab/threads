c39f09a0fd323dd6b064330e7826bfec34bf18b1
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2025-04-15 12:49:38 +0000

    fix: add start and end to instructions pickle (#51)
    - Bug found whilst adding included test, which highlighted a nuance where `end` was not necessarily derivable from positions, so start and end have been added explicitly to the tuple.

--- src/pybind_utils.cpp
+++ src/pybind_utils.cpp
@@ -22,8 +22,8 @@
 namespace py = pybind11;
 
 py::tuple threading_instructions_get_state(const ThreadingInstructions& ti) {
-    // Instructions may be stored as a 2-tuple with first arg being a list
-    // of per-instruction data, and the second being the positions.
+    // Instructions stored as a 4-tuple consisting of list of per-instruction
+    // data, positions, start and end.
     py::list inst_list;
     for (const auto& inst : ti.instructions) {
         inst_list.append(
@@ -36,11 +36,11 @@ py::tuple threading_instructions_get_state(const ThreadingInstructions& ti) {
         );
     }
 
-    return py::make_tuple(inst_list, ti.positions);
+    return py::make_tuple(inst_list, ti.positions, ti.start, ti.end);
 }
 
 ThreadingInstructions threading_instructions_set_state(py::tuple tup) {
-    // Unpack the 2-tuple into threading instructions and positions
+    // Unpack the 4-tuple into threading instructions and positions
     std::vector<ThreadingInstruction> insts;
     py::list inst_list = tup[0];
     for (py::handle inst : inst_list) {
@@ -55,5 +55,7 @@ ThreadingInstructions threading_instructions_set_state(py::tuple tup) {
 
     std::vector<int> positions = tup[1].cast<std::vector<int>>();
     ThreadingInstructions instructions{insts, positions};
+    instructions.start = tup[2].cast<int>();
+    instructions.end = tup[3].cast<int>();
     return instructions;
 }
