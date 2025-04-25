3e3d3bf781c252f39e5f637ab7ea507c538de956
Arni Gunnarsson <awo066@cluster1.hpc.in.bmrc.ox.ac.uk>
2025-04-25 22:46:38 +0100

    rename parallelism

--- src/threads_arg/__main__.py
+++ src/threads_arg/__main__.py
@@ -83,8 +83,7 @@ def convert(**kwargs):
 @main.command()
 @click.option("--threads", required=True, help="Path to an input .threads file.")
 @click.option("--out", required=True, help="Path to output.")
-@click.option("--num_batches", type=int, help="Number of batches to split threads into", default=None)
-@click.option("--num_processors", type=int, help="Size of processor pool to process batches", default=None)
+@click.option("--num_threads", type=int, help="Size of processor pool to process batches", default=None)
 def allele_ages(**kwargs):
     from .allele_ages import estimate_allele_ages
     estimate_allele_ages(**kwargs)
